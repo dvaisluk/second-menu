994a8a1aae3219c6dd1a0a2affaed2c49166373d
dvaisluk <70915114+dvaisluk@users.noreply.github.com>
2021-11-06 00:41:22 +0300

    Add files via upload

---
+++ 2 задание/MainActivity2.java
@@ -0,0 +1,46 @@
+package com.example.md172;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_manu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.first:
+
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.second:
+
+                startActivity(new Intent(this, MainActivity2.class));
+                break;
+            case R.id.Third:
+
+                startActivity(new Intent(this, MainActivity3.class));
+                break;
+            case R.id.Fourth:
+
+                startActivity(new Intent(this, MainActivity4.class));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
